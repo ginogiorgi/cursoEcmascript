8f4b7095ce3325561c69e14ff6eeb71056cbd01d
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-11 08:34:58 -0300

    Clase 5 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ src/es6/02-strings.js
@@ -0,0 +1,20 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+
+console.log(epicPhrase);
+
+//template Literals
+let epicPhrase2 = `${hello} ${world}`;
+
+console.log(epicPhrase2);
+
+//Multi-line strings
+let lorem = 'Esto es un string \n' + 'Esto es otra linea';
+
+console.log(lorem);
+
+let lorem2 = `Esta es una frase epica.
+La continuación de esta frase epica.`;
+
+console.log(lorem2);
