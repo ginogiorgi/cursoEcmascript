778460c3995ed3dd4880e67e6d6c3a49e65f1627
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-14 00:20:48 -0300

    Clase 10 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ 05-object-literals.js
@@ -0,0 +1,12 @@
+// enhanced object literals
+
+function newUser(user, age, country, id) {
+    return {
+        user,
+        age,
+        country,
+        id,
+    };
+};
+
+console.log(newUser("chese890", 24, "ARG", 1));
