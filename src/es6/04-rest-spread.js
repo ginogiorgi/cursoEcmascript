762438624413d5becc68a0184262b2e1a979b8bb
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-13 23:47:23 -0300

    Clase 8 Curso de ECMAScript: Historia y Versiones de JavaScript

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -1,4 +1,4 @@
-//arrays destructuring
+//Arrays destructuring
 
 let fruits = ["manzana", "banana", "naranja"];
 let [a,b] = fruits;
@@ -6,10 +6,44 @@ let [a,b] = fruits;
 console.log(a, b);
 console.log(a, fruits[1]);
 
-//object destructuring
+//Object destructuring
 
 let user = {username: "Gino", age: "24"};
 let {username, age} = user
 
 console.log(username, age);
 console.log(username, user.age);
+
+//Spread operator 
+
+let person = {name: "Gino", age: 28};
+let country = "ARG";
+
+let data = { id: 1, ...person, country};
+
+console.log(data);
+
+//Rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4, 5);
+
+json1 = {
+    name: 'Mr. Michi',
+    food: 'Pescado',
+    },
+json2 = {
+      age: 12,
+      color: 'Blanco',
+    }
+function solution(json1, json2){
+    let data = { ...json1, ...json2 }
+    return data;
+}
+
+solution(json1, json2);
