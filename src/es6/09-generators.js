b6dc5033f44c1381527edc5f2ca819e14a93166b
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-14 20:01:03 -0300

    Clase 14 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ src/es6/09-generators.js
@@ -0,0 +1,8 @@
+function* iterate(arr) {
+    for (let value of arr) {
+        yield value;
+    }
+} 
+
+const it = iterate(["Oscar", "David", "Ana", "Ulises", "Jennifer"]);
+console.log(it.next().value); 
