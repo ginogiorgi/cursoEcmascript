3e7c37c2bfe6ff2d6be1cdc7b83930ab1a443753
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-15 00:23:41 -0300

    Clase 18 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2 , 3, 4, 5, 6, 7, 8];
+
+console.log(numbers.includes(4));
+
+const list = ["Oscar", "David", "Ana"];
+
+console.log(list.includes("Oscar"));
+console.log(list.includes("oscar"));
