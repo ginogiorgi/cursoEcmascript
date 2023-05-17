c956c81696f80226ac53f0fff04411ca4f61beea
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-17 09:31:05 -0300

    Clase 31 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ src/es12/01-replace-all.js
@@ -0,0 +1,6 @@
+const string = "Poronga sucia y ricoteada"
+
+const replacedString = string.replace("sucia", "limpia");
+const replacedString2 = replacedString.replace("ricoteada", "perfumada");
+
+console.log(replacedString2);
