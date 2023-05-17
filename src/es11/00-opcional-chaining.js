0f69cd4631b7567e4c3d9f88a0200ddcb3a01684
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-17 07:38:46 -0300

    Clase 26 Curso de ECMAScript: Historia y Versiones de JavaScript

---
+++ src/es11/00-opcional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+    chese890: {
+        country: "ARG",
+    },
+    ana: {
+        country: "ARG",
+    },
+};
+console.log(users.chese890.country);
+console.log(users.chese890.age);
+console.log(users?.bebeloper?.country);
